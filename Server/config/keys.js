7a0bb785c830061fcb10f6a17785e2da309a1425
amrita jassal <amrita.singh@trainingmug.com>
2023-08-24 22:32:31 +0530

    Server

---
+++ Server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports={
+    MONGOURI:"mongodb+srv://amrita:hEcF6q9QUuuExkJY@cluster0.ewt3qx1.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"efefefefefefe1",//should be unique 
+}
+
+
+// hEcF6q9QUuuExkJY
